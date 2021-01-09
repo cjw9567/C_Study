ca915d79ce4c1a06eb2e584da4a14869636350cf
진욱 최 <7wind7@naver.com>
2021-01-09 21:01:24 +0900

    선택형 계산기 - 항상출력되는 결과 수정

--- calculator02/calculator02.c
+++ calculator02/calculator02.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 typedef struct _Data {
-    double num1;
-    double num2;
-    double res;
-    int sel;
+    double num1; //실수형변수 1
+    double num2; //실수형변수 2
+    double res;  // 연산결과를 저장
+    int sel; //연산자
 }Data;
 
 
@@ -24,10 +24,12 @@ void result(Data* dat) {
         dat->res = dat->num1 / dat->num2;
         break;
     default:
-        printf("올바르지 않은 선택 \n");
+        dat->res = 123;
         break;
     }
+
 }
+
 
 
 int main(void) {
@@ -39,12 +41,30 @@ int main(void) {
     scanf_s("%lf", &dat.num2);
 
     system("cls");
-    printf("입력된 수 : (%g, %g) \n", dat.num1, dat.num2);
+    printf("입력된 수 : [%g], [%g] \n", dat.num1, dat.num2);
     printf("더하기(1), 빼기(2), 곱하기(3), 나누기(4) \n");
     printf("연산 선택 : ");
     scanf_s("%d", &dat.sel);
 
-    result(&dat);
-    printf("결과 : %g \n", dat.res);
-    return 0;
+
+    if (dat.sel == 1) {
+        result(&dat);
+        printf("결과 : %g \n", dat.res); // 참일 경우에 실행될 문장 
+    }
+    else if (dat.sel == 2) {
+        result(&dat);
+        printf("결과 : %g \n", dat.res); // 참일 경우에 실행될 문장 
+    }
+    else if (dat.sel == 3) {
+        result(&dat);
+        printf("결과 : %g \n", dat.res); // 참일 경우에 실행될 문장 
+    }
+    else if (dat.sel == 4) {
+        result(&dat);
+        printf("결과 : %g \n", dat.res); // 참일 경우에 실행될 문장 
+    }
+    else {
+        printf("올바르지 않은 선택입니다. \n결과값을 출력 할 수 없습니다. \n"); // 위의 조건식에 아무것도 해당하지 않을때 실행될 문장 
+    }
+
 }
